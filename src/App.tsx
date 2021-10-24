57dab8e4a2ce8ecbc205557cbfd54b052647ca50
bregarulai <bregarulai@gmail.com>
2021-10-24 15:45:47 -0400

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import User from "./components/User";
+import Users from "./components/Users";
+import Landing from "./components/Landing";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/",
@@ -12,9 +14,16 @@ const client = new ApolloClient({
 const App = () => {
   return (
     <ApolloProvider client={client}>
-      <div>
-        <User />
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Users />
+          </Route>
+          <Route path="/landing">
+            <Landing />
+          </Route>
+        </Switch>
+      </Router>
     </ApolloProvider>
   );
 };
--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -14,18 +14,18 @@ interface User {
   name: string;
 }
 
-const User = () => {
+const Users = () => {
   const { loading, error, data } = useQuery(USER_QUERY);
   if (loading) return <p>Loading...</p>;
   if (error) return <p>{error.message}</p>;
-  console.log("Data: ", data);
+
   return (
     <div>
-      {data.users.map((user: User) => {
-        return <p>{user.name}</p>;
+      {data.users.map((user: User, index: number) => {
+        return <p key={index}>{user.name}</p>;
       })}
     </div>
   );
 };
 
-export default User;
+export default Users;
